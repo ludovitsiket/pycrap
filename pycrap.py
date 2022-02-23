836c034fca0881c7d87e9d5b44b091ea18db0ee0
Ludovit <siket.ludovit@gmail.com>
2022-02-23 15:44:48 +0100

    Add functions address, count_lines, read_urls.
    gitignore file.
    Edited function syntax().
    Working ping.

--- pycrap.py
+++ pycrap.py
@@ -5,21 +5,42 @@ import sys
 def pings(addr, num):
     i = 0
     while i < num:
-        x = subprocess.check_output(["ping", "-c", "1", addr])
-        print(x)
+        print(subprocess.check_output(["ping", "-c", "1", addr]))
         i = i+1
 
 
 def syntax():
-    print('Syntax: python3 pycrap.py <ADDRESS> <number_of_pings>')
+    print('Syntax: python3 pycrap.py <number_of_pings>')
+    print('ip.txt contain address for ping')
+
+
+def count_lines(file_name):
+    with open(file_name) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
+def read_urls(some_file, number, result):
+    i = 0
+    with open(some_file, 'r') as f:
+        while i < number:
+            value = f.readline().replace('\n', '')
+            result.append(value)
+            i += 1
+    return result
+
+
+def address(addr):
+    i = 0
+    address = read_urls(addr, count_lines(addr), [])
+    return address[i]
 
 
 def main():
     try:
-        ip = sys.argv[1]
-        num = int(sys.argv[2])
-        pings(ip, num)
-    except (IndexError, ValueError) as e:
+        pings(address('ip.txt'), int(sys.argv[1]))
+    except (IndexError, ValueError, subprocess.CalledProcessError) as e:
         print(e)
         syntax()
 
